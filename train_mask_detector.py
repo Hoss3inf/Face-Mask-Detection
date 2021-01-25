73c337ddd2470c8c478554a9596a994a666cc2eb
HOss3inf <hossein.2006man@gmail.com>
2021-01-25 09:44:10 +0330

    added farsi desc to train_masak_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,7 +1,9 @@
 # USAGE
+# با این کد می توان فایل را اجرا کرد
 # python train_mask_detector.py --dataset dataset
 
 # import the necessary packages
+# وارد کردن پکیج های مورد نیاز
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import AveragePooling2D
@@ -37,32 +39,39 @@ args = vars(ap.parse_args())
 
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
+# راه اندازی نرخ یادگیری، تعداد ایپوک ها و سایز بچ
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
+# اطلاعات دیتاست را، شامل دیتا و لیبل ها را می خوانیم
 print("[INFO] loading images...")
 imagePaths = list(paths.list_images(args["dataset"]))
 data = []
 labels = []
 
 # loop over the image paths
+# یک حلقه for روی همه عکس ها اجرا می کنیم
 for imagePath in imagePaths:
 	# extract the class label from the filename
+	با استفاد از اسم فایل، لیبل عکس را تشحیص می دهیم
 	label = imagePath.split(os.path.sep)[-2]
 
 	# load the input image (224x224) and preprocess it
+	# عکس را لود می کنیم و پیش پردازش روی ان انجام میدهیم
 	image = load_img(imagePath, target_size=(224, 224))
 	image = img_to_array(image)
 	image = preprocess_input(image)
 
 	# update the data and labels lists, respectively
+	# لیبل و عکسی که پیش پردازش شده است را به لیست دیتا و لیبل اضافه می کنیم
 	data.append(image)
 	labels.append(label)
 
 # convert the data and labels to NumPy arrays
+# لیست دیتا و لیبل ها را به آرایه  numpy تبدیل می کنیم
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
@@ -73,10 +82,12 @@ labels = to_categorical(labels)
 
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
+# دیتاست را به دو بخش تقسیم می کنیم. ۷۵ درصد برای آموزش دادن، و ۲۵ درصد برای تست کردن
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
 	test_size=0.20, stratify=labels, random_state=42)
 
 # construct the training image generator for data augmentation
+# شرایط و محدودیت های عکس ها را تعیین می کنیم برای زمان فیت کردن مدل
 aug = ImageDataGenerator(
 	rotation_range=20,
 	zoom_range=0.15,
@@ -88,11 +99,13 @@ aug = ImageDataGenerator(
 
 # load the MobileNetV2 network, ensuring the head FC layer sets are
 # left off
+# از شبکه عصبی MobileNetV2 استفاده کرده، شبکه ای که برای استفاده در دستگاه های موبایل بهینه شده
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
 # construct the head of the model that will be placed on top of the
 # the base model
+
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -125,18 +138,23 @@ H = model.fit(
 	epochs=EPOCHS)
 
 # make predictions on the testing set
+# از مدل بر روی دیتاست تست استفاده می کنیم
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
+# ایندکس عکس ها را بر اساس بزرگترین احتمالی که مدل تفیین کرده معین می کنیم
+#(مثلا احتمالا با ماسک بودن ۶۰ درصد و احتمال بی ماسک بودن ۵۵ درصد است. پس در دسته ی با ماسک قرار میدهد عکس را)
 predIdxs = np.argmax(predIdxs, axis=1)
 
 # show a nicely formatted classification report
+# یک گزارش قشنگ چاپ می کنیم
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
 
 # serialize the model to disk
+# مدل را در دیسک ذخیره می کنیم
 print("[INFO] saving mask detector model...")
 model.save(args["model"], save_format="h5")
 
