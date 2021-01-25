0688abdc70ff5ab469c565d8a706d239d1353552
HOss3inf <hossein.2006man@gmail.com>
2021-01-25 14:39:16 +0330

    annotated detect_mask_image.py

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -26,6 +26,7 @@ def mask_image():
 	args = vars(ap.parse_args())
 
 	# load our serialized face detector model from disk
+	#مدل تشخیص چهره را از دیسک می خوانیم
 	print("[INFO] loading face detector model...")
 	prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
 	weightsPath = os.path.sep.join([args["face"],
@@ -33,45 +34,58 @@ def mask_image():
 	net = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 	# load the face mask detector model from disk
+	# مدل تشخیص ماسک را از دیسک می خوانیم
 	print("[INFO] loading face mask detector model...")
 	model = load_model(args["model"])
 
 	# load the input image from disk, clone it, and grab the image spatial
 	# dimensions
+	# عکس ها را از حافظه خوانده، و ابعاد فضایی آن را مشخص می کنیم (مثلا در اینجا دو بعد)
 	image = cv2.imread(args["image"])
 	orig = image.copy()
 	(h, w) = image.shape[:2]
 
 	# construct a blob from the image
+	#عکس را تبدیل به blob می کنیم
 	blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
 		(104.0, 177.0, 123.0))
 
 	# pass the blob through the network and obtain the face detections
+	# بلاب ها را به شبکه عصبی می دهیم و تشخیص اینکه چهره در عکس هست یا نه را از شبکه می گیریم
+	# دقت کنید که چهره را تشخیص می دهیم در این قدم، نه ماسک را
 	print("[INFO] computing face detections...")
 	net.setInput(blob)
 	detections = net.forward()
 
 	# loop over the detections
+	# لوپ روی تشخیص ها
 	for i in range(0, detections.shape[2]):
 		# extract the confidence (i.e., probability) associated with
 		# the detection
+		# احتمال اینکه چهره در عکس باشد را استخراج می کنیم
 		confidence = detections[0, 0, i, 2]
 
 		# filter out weak detections by ensuring the confidence is
 		# greater than the minimum confidence
+		# اگر احتمال وجود چهره از حدی پایینتر باشد، عکس را در نظر نمی گیریم
+
 		if confidence > args["confidence"]:
 			# compute the (x, y)-coordinates of the bounding box for
 			# the object
+			#مختصات ایکس و ایگرگ باکس حاوی چهره را تشخیص می دهیم
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 
 			# ensure the bounding boxes fall within the dimensions of
 			# the frame
+			# مطمین می شویم که محتصات یافت شده، در ابعاد عکس هست و خارج از آن نیست
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
 			# extract the face ROI, convert it from BGR to RGB channel
 			# ordering, resize it to 224x224, and preprocess it
+			# چهره را استخراج می کنیم، کانال آن را از bgr به  rgb تغییر می دهیم و
+			# اندازه آن را به ۲۲۴ در ۲۲۴ تغییر می دهیم
 			face = image[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 			face = cv2.resize(face, (224, 224))
@@ -81,23 +95,28 @@ def mask_image():
 
 			# pass the face through the model to determine if the face
 			# has a mask or not
+			# چهره را از مدل رد می کنیم تا ببینیم ماسک به صورت دارد یا نه
 			(mask, withoutMask) = model.predict(face)[0]
 
 			# determine the class label and color we'll use to draw
 			# the bounding box and text
+			# کلاسی که از روی آن باید رنگ و قالب جعبه دور چهرهر قرار بگیرد را مشخص می کند
 			label = "Mask" if mask > withoutMask else "No Mask"
 			color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
 			# include the probability in the label
+			# احتمال اینکه ماسک داشته باشد را در لیبل قید می کند
 			label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
 			# display the label and bounding box rectangle on the output
 			# frame
+			# نمایش باکس و احتمال ماسک داشتن بر روی عکس نهایی
 			cv2.putText(image, label, (startX, startY - 10),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 			cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
 
 	# show the output image
+	# نمایش عکس نهایی
 	cv2.imshow("Output", image)
 	cv2.waitKey(0)
 	
